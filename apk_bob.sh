c32ec4436284dd5fe592a11230df37a7fe537ddd
Natesh Relhan <nateshrelhan@gmail.com>
2017-04-21 10:15:04 +0530

    directory bug fix

--- apk_bob.sh
+++ apk_bob.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-function copyBuildToTagFolder {  
+function buildCommandIndentifier {
+        echo "----"$buildType"----"
+	echo "----ClEAN----"
+        (cd ~/curofy && $cleanCommand)
+	echo "----BUILD----"
+	if [[ $buildType == "PRODUCTION" ]]; then
+	        buildCommand="assembleProductionDebug"
+	else
+	        buildCommand="assembleStagingDebug"
+	fi			
+}
+function copyBuildToTagFolder { 
         mkdir -p $newDir
 	echo "----COPYING----"
 	(cd ~/Brandon/ &&  cp -a ~/curofy/presentation/build/outputs/apk/* $newDir"/")
+	echo "----DIRECTORY: "ls $newDir"----"
 }
-
-
 echo "----BUILD APK STARTED-----"
 inputValue=${1,,}
 inputTagValue=${2,,}
@@ -13,38 +23,29 @@ cleanCommand="bash gradlew clean"
 fetchTag=$( cd ~/curofy && git fetch --tags )
 if [[ $inputValue == *"prod"* ]]; then
 	buildType="PRODUCTION"
-        echo "----"$buildType"----"
-	echo "----ClEAN----"
-        (cd ~/curofy && $cleanCommand)
-	echo "----BUILD----"
-        buildCommand="assembleProductionDebug"
 elif [[ $inputValue == *"stag"* ]]; then
 	buildType="STAGING"
-        echo "----"$buildType"----"
-	echo "----CLEAN----"
-        (cd ~/curofy && $cleanCommand)
-	echo "----BUILD----"
-        buildCommand="assembleStagingDebug"
 fi
+buildCommandIndentifier
 if [[ ! -z $buildCommand ]]; then
+	echo "----Fetching....----"
         if [[ ! -z $inputTagValue ]]; then
-		echo "----Fetching....----"
                 $fetchTag
                 echo "----Git checking out... -"$inputTagValue"----"
                 ( cd ~/curofy && git checkout $inputTagValue )
                 tagFolder=$buildType"_"$inputTagValue
         else
-		echo "----Fetching....----"
                 echo "----Git checking out... -development""----"
                 ( cd ~/curofy && git fetch origin development && git checkout development )
                 latestCommitHash=$(git log -n 1 | grep "commit" | awk '{print $2}')
                 tagFolder=$buildType"_development_"$latestCommitHash
         fi
   	newDir=~/"Brandon/builds/"$tagFolder
-	echo "----DIRECTORY: "$newDir"----"
 	if [[ ! -d $newDir ]]; then
         	( cd ~/curofy && bash gradlew $buildCommand )
-	      	trap copyBuildToTagFolder EXIT			
+	      	trap copyBuildToTagFolder EXIT		
+	else
+		echo "----DIRECTORY: "ls $newDir"----"
 	fi
 else
         echo "Please enter correct build type"
